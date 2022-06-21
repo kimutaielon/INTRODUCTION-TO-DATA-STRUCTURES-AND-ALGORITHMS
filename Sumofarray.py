f2238886e86f9db700658f38cb124a1713339cc3
kimutaielon <104800433+kimutaielon@users.noreply.github.com>
2022-06-21 10:59:59 +0300

    Add files via upload

---
+++ Sumofarray.py
@@ -0,0 +1,13 @@
+import math
+import os
+import random
+import re
+import sys
+
+def simpleArraySum(ar):
+    return(sum(ar))
+ar = []
+ar = [23,40,47,12,91,29,50]
+n = len(ar)
+ans = sum(ar,n)
+print("Sum of array is: " ,ans)
